752dbfbc0f88de74d33946b6640d4d0255f56086
ANKIT128912 <ankit121ab@gmail.com>
2026-03-15 23:57:46 +0530

    fix api base url

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://vercel-backened.onrender.com"
+  baseURL: "https://vercel-backened.onrender.com/api"
 });
 
 export default API;
