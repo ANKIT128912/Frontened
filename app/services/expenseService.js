4f348d6d5982b2f2584e40445fb9178a81b05e59
ANKIT128912 <ankit121ab@gmail.com>
2026-03-15 23:17:42 +0530

    fix expense api routes

--- app/services/expenseService.js
+++ app/services/expenseService.js
@@ -1,42 +1,29 @@
 import axios from "axios";
 
-// Your deployed backend URL
 const API = "https://vercel-backened.onrender.com/api/expenses";
 
 // Add Expense
 export const addExpense = async (expenseData) => {
-  try {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    const res = await axios.post(`${API}/add`, expenseData, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  const res = await axios.post(`${API}/add`, expenseData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    return res.data;
-
-  } catch (error) {
-    console.log(error.response?.data);
-    throw error;
-  }
+  return res.data;
 };
 
 // Get Expenses
 export const getExpenses = async () => {
-  try {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    const res = await axios.get(`${API}/get`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  const res = await axios.get(API, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    return res.data;
-
-  } catch (error) {
-    console.log(error.response?.data);
-    throw error;
-  }
+  return res.data;
 };
